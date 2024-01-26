6fbb88d84c6d3e37c1232c0d59180e587e41a8e4
Hisoka37 <walidkabou@gmail.com>
2024-01-26 16:00:27 +0100

    isntall redis, setup turbo

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ApplicationRecord
     validates_uniqueness_of :name
     scope :public_rooms, -> {where(is_private: false)}
+    after_create_commit { broadcast_append_to "rooms"}
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   scope :all_except, -> (user) {where.not(id: user)}
+  after_create_commit { broadcast_append_to "users"}
 end
