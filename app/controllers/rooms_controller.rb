d70a1e942667f6baf06cac4a6b280fd495b85f12
Hisoka37 <walidkabou@gmail.com>
2024-01-26 16:19:49 +0100

    Add form to create new rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,23 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
 
   def index 
+    @room = Room.new
     @rooms = Room.public_rooms
     @users = User.all_except(current_user)
   end
+
+  def create
+    @room = Room.create(room_params)  
+  end
+
+
+
+  
+  private 
+
+  def room_params
+    params.require(:room).permit(:name)
+  end
 end
+
+
