45a7460a16d2cb68b4fedb3efa3e6c63fb3eb5dd
Salwa99 <s.ritage99@gmail.com>
2024-02-01 16:24:07 +0100

    Adding active stotage Images to  devise model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,20 @@ class User < ApplicationRecord
   scope :all_except, -> (user) {where.not(id: user)}
   after_create_commit { broadcast_append_to "users"}
   has_many :messages
+  has_one_attached :avatar
+
+  after_commit :add_default_avatar, on: %i[create update]
+
+
+  private
+
+  def add_default_avatar
+    return if avatar.attached?
+
+    avatar.attach(
+      io: File.open(Rails.root.join('app', 'assets', 'images', 'default_profile.jpeg')),
+      filename: 'default_profile.jpeg',
+      content_type: 'image/jpeg'
+    )
+  end
 end
