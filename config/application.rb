36f12ed31a5eb2f844942d2dddfba07bfa04976a
Salwa99 <s.ritage99@gmail.com>
2024-02-02 18:00:28 +0100

    Creating realtime user status

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module ChatApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    config.after_intialize do |_config|
+      User.update_all(status: User.statuses[:offline])
+    end
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_01_150425) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_02_155646) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,6 +76,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_01_150425) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
