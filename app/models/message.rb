c32a2426449cfeedb1dcacd23cbd5820853d9f7d
Hisoka37 <walidkabou@gmail.com>
2024-02-07 13:40:20 +0100

    Style the message attachements holder

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,18 @@ class Message < ApplicationRecord
   belongs_to :room
   after_create_commit {broadcast_append_to self.room}
   before_create :comfirm_participant
+  has_many_attached :attachments, dependent: :destroy
+
+  def chat_attachment(index)
+    target = attachments[index]
+    return unless attachments.attached?
+
+    if target.image?
+      target.variant(resize_to_limit: [150, 150]).processed
+    elsif target.video?
+      target.variant(resize_to_limit: [150, 150]).processed
+    end
+  end
 
   def comfirm_participant
     return unless room.is_private
