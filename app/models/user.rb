eb8b2cf2a6248cc1e53aec21c4fc28ceb01cf29c
Hisoka37 <walidkabou@gmail.com>
2024-02-13 16:23:19 +0100

    Add roomshelper : autocomplete search

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,18 @@ class User < ApplicationRecord
   after_update_commit { broadcast_update}
   has_many :messages
   has_one_attached :avatar
+  has_many :joinables, dependent: :destroy
+  has_many :joined_users, through: :joinables, source: :user
 
+  enum role: %i[user admin]
   enum status: %i[offline away online]
 
+
   after_commit :add_default_avatar, on: %i[create update]
+  after_initialize :set_default_role, if: :new_record?
+
+
+
 
   def avatar_thumbnail
     avatar.variant(resize_to_limit: [150, 150]).processed
@@ -49,4 +57,9 @@ class User < ApplicationRecord
       content_type: 'image/jpeg'
     )
   end
+
+  def set_default_role
+    self.role ||= :user
+  end
+
 end
