a9804fb476eff808fdcadd56f0b00e82141ebcea
Hisoka37 <walidkabou@gmail.com>
2024-02-14 12:29:37 +0100

    Creating the search helper

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,22 +1,25 @@
 class RoomsController < ApplicationController
+  include RoomsHelper
   before_action :authenticate_user!
   after_action :set_status
 
-  def index 
+  def index
     @room = Room.new
-    @rooms = Room.public_rooms
+    @rooms = Room.search_rooms
+    @joinded_rooms = current_user.joined_rooms
     @users = User.all_except(current_user)
     render 'index'
   end
 
-  def show 
+  def show
     @single_room = Room.find(params[:id])
     @room = Room.new
-    @rooms = Room.public_rooms
+    @rooms = Room.search_rooms
+    @joinded_rooms = current_user.joined_rooms
 
     @message = Message.new
 
-    pagy_messages = @single_room.messages.order(created_at: :desc)
+    pagy_messages = @single_room.messages.includes(:user).order(created_at: :desc)
     @pagy, messages = pagy(pagy_messages, items: 10)
     @messages = messages.reverse
 
@@ -25,10 +28,35 @@ class RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.create(room_params)  
+    @room = Room.create(room_params)
   end
 
-  private 
+  def search
+    @rooms = search_rooms
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.update('search_results',
+                              partial: 'rooms/search_results',
+                              locals: { rooms: @rooms })
+        ]
+      end
+    end
+  end
+
+  def join
+    @room = Room.find(params[:id])
+    current_user.joined_rooms << @room
+    redirect_to @room
+  end
+
+  def leave
+    @room = Room.find(params[:id])
+    current_user.joined_rooms.delete(@room)
+    redirect_to rooms_path
+  end
+  
+  private
 
   def room_params
     params.require(:room).permit(:name)
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
   resources :rooms do
     resources :messages
+    collection do
+      post :search
+    end
   end
+    # leave_room_path(room)
+    get 'rooms/leave/:id', to: 'rooms#leave', as: 'leave_room'
+    # join_room_path(room)
+    get 'rooms/join/:id', to: 'rooms#join', as: 'join_room' 
+    
   root 'pages#home'
   devise_for :users, controllers: {
     sesions: 'users/sessions',
     registrations: 'users/registrations'
   }
- 
-  get 'user/:id', to: 'users#show', as: 'user'
 
+  get 'user/:id', to: 'users#show', as: 'user'
 end
