4e49a94ae12288d04dfbad09fa4493d2afd57961
Hisoka37 <walidkabou@gmail.com>
2024-02-21 20:18:27 +0100

    update room/user partials and generate user/room stimulus controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,10 +20,5 @@ class UsersController < ApplicationController
     render 'rooms/index'
   end
 
-  private
-
-  def get_name(user1, user2)
-    user = [user1, user2].sort
-    "private_#{user[0].id}_#{user[1].id}"
-  end
+  
 end
